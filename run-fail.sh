864853ebb9cf409a91bdd1293e6cc59e868a1cb5
Alessandro Scala <alessandro.scala8@gmail.com>
2023-08-20 22:19:01 +0200

    Imported semantic analysis files

--- run-fail.sh
+++
@@ -1,6 +0,0 @@
-make build
-
-for f in test/samples/fail*
-do
-    dune exec _build/default/test/parser_test.exe $f 2> \dev\null
-done
--- run-tests.sh
+++
@@ -1,6 +0,0 @@
-make build
-
-for f in test/samples/test*
-do
-    dune exec _build/default/test/parser_test.exe $f 1> \dev\null
-done
