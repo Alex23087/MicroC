0b8763b9d9c289ba4e486aed512904f1ad3757bb
Alessandro Scala <alessandro.scala8@gmail.com>
2023-08-20 22:12:37 +0200

    Test scripts

---
+++ run-fail.sh
@@ -0,0 +1,6 @@
+make build
+
+for f in test/samples/fail*
+do
+    dune exec _build/default/test/parser_test.exe $f 2> \dev\null
+done
---
+++ run-tests.sh
@@ -0,0 +1,6 @@
+make build
+
+for f in test/samples/test*
+do
+    dune exec _build/default/test/parser_test.exe $f 1> \dev\null
+done
