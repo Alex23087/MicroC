64b2dd1ec32ad45de962b000b3baf40a81dce063
Alessandro Scala <alessandro.scala8@gmail.com>
2023-08-31 15:39:21 +0200

    Codegen

--- lib/codegen.ml
+++ lib/codegen.ml
@@ -9,6 +9,8 @@ let (>>) f g x = g(f(x))
 (* let (<<) f g x = f(g(x)) *)
 
 let ctx = global_context()
+let counter = ref 0
+let get_unique_name () = let i = !counter in incr counter >. i |> string_of_int
 
 let rec typ_to_llvmtype typ = (
   match typ with
@@ -26,7 +28,7 @@ let rec typ_to_llvmtype typ = (
 
 let add_global_var mcmodule sym_table (typ, id) =
   sym_table |> (add_entry id (
-    Llvm.declare_global (typ_to_llvmtype typ) id mcmodule)
+    Llvm.define_global id (typ |> typ_to_llvmtype |> const_null) mcmodule)
   ) >. ()
 
 let add_function_to_module mcmodule sym_table fundecl =
@@ -37,7 +39,7 @@ let add_function_to_module mcmodule sym_table fundecl =
     _
   } = fundecl in
   sym_table |> (add_entry fname (
-    Llvm.declare_function fname (
+    Llvm.define_function fname (
       Llvm.function_type (typ_to_llvmtype typ) (
         formals |>
         List.map ((fun (typ, _) -> typ) >> typ_to_llvmtype) |>
@@ -53,7 +55,55 @@ let add_topdecl_to_module mcmodule sym_table topdecl =
     | Vardec (typ, id) -> add_global_var mcmodule sym_table (typ, id)
 
 
-let build_function _ = ()
+let rec build_function mcmodule sym_table fd = let {fname; formals; body; _} = fd in
+  let fundef = lookup fname sym_table in
+  let builder = Llvm.builder_at_end ctx (Llvm.entry_block fundef) in
+
+  sym_table |> begin_block >. ();
+  formals |> List.iteri (
+    fun i (_, id) -> add_entry id (Llvm.param fundef i) sym_table >. ());
+
+  build_stmt mcmodule sym_table builder body;
+
+and build_stmt mcmodule sym_table builder stmt =
+  match (@!) stmt with
+    | If _ -> failwith "Not Implemented"
+    | While _ -> failwith "Not Implemented"
+    | Expr e -> build_expr mcmodule sym_table builder e (TypP TypV)>. ()
+    | Return _ -> failwith "Not Implemented"
+    | Block block -> build_block mcmodule sym_table builder block
+
+and build_block mcmodule sym_table builder block =
+  let sym_table = sym_table |> begin_block in
+  block |> List.iter (fun sd -> (
+    match (@!) sd with
+      | Dec (typ, id) -> build_local_decl sym_table builder (typ, id)
+      | Stmt stmt -> build_stmt mcmodule sym_table builder stmt
+  ));
+  sym_table |> end_block >. ()
+
+and build_expr mcmodule sym_table builder expr nulltype =
+  match (@!) expr with
+    | Access _ -> failwith "Not Implemented"
+    | Assign _ -> failwith "Not Implemented"
+    | Addr _ -> failwith "Not Implemented"
+    | ILiteral i -> Llvm.const_int (TypI |> typ_to_llvmtype) i
+    | CLiteral c -> Llvm.const_int (TypC |> typ_to_llvmtype) (c |> int_of_char)
+    | BLiteral b -> Llvm.const_int (TypB |> typ_to_llvmtype) (b |> Bool.to_int)
+    | Nullptr -> Llvm.const_null (nulltype |> typ_to_llvmtype)
+    | UnaryOp (uop, expr) -> (
+      let exprval = build_expr mcmodule sym_table builder expr nulltype in
+      match uop with
+        | Not -> Llvm.build_neg exprval (get_unique_name()) builder
+        | Neg -> Llvm.build_neg exprval (get_unique_name()) builder
+    )
+    | BinaryOp _ -> failwith "Not Implemented"
+    | Call _ -> failwith "Not Implemented"
+
+and build_local_decl sym_table builder (typ, id) =
+  let vartyp = typ_to_llvmtype typ in
+  let llvar = Llvm.build_alloca vartyp id builder in
+  sym_table |> add_entry id llvar >. ()
 
 let to_llvm_module program =
   match program with
@@ -66,7 +116,7 @@ let to_llvm_module program =
 
       topdecls |>
       List.filter_map (fun td -> match (@!) td with | Fundecl fd -> Some fd | _ -> None) |>
-      List.iter build_function;
+      List.iter (build_function mcmodule sym_table);
 
       mcmodule
     )
